c1fb6699af7756b69484d32026c0de167ea37b4e
bele <belesemu@gmail.com>
2023-06-02 23:11:50 -0500

    add shoping cart html and css

---
+++ boolean.js
@@ -0,0 +1,34 @@
+if("" || undefined || null || 0){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+let x = 5;
+console.log(!x)
+
+let s = "connect bele"
+console.log(s)
+let str1 = "hello"
+let str2 = 'hi'
+let str3 = "john say " + "hi"
+
+let str5 = 'hi "says john"'
+console.log(str5)
+
+let str7 = '' +
+    '|' +
+    '' +
+    '';
+let co= 'bele'
+let wel = 'welcome ${}'
+console.log(wel)
+//.length is a property in js unlike java which is a method
+// undefined a vaariable withoiut a  value
+let i;
+// reference error whenever we try to access a value that is not defined
+console.log(a)// reference error
+console.log(i.length)// type error
+
+console.log(i)
+
---
+++ main.js
@@ -0,0 +1,21 @@
+var m = 88;
+console.log(m)
+
+
+console.log(typeof m);
+for (let i=0;i<5;i++){
+    console.log(i);
+}
+//console.log("outside loop: " ,i);
+
+const obj ={
+    firstName : 'john',
+    lname : 'smith'
+}
+console.log(obj.firstName)
+obj.firstName = 'Edmaward'
+console.log(obj.firstName)
+console.log(obj.lname)
+
+// all  the number in java script are number type
+
